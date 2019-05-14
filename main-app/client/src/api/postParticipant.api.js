caca06b850de332a60406a1458d986f70d5a6d65
Kevin Crawford <kcdouglas92@gmail.com>
2019-05-14 10:19:07 -0700

    created formsubmit handler and postParticipant api call, logging response and error

---
+++ main-app/client/src/api/postParticipant.api.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+import { API_BASE_URL } from "../config/url_config";
+
+import { UserAuth } from "../utilities/auth";
+
+const postParticipant = (participant, successFn, errorFn) => {
+  // get authorization token from UserAuth to authenticate that
+  // user is logged in
+  const authToken = UserAuth.getAuthToken();
+  let config = {
+    headers: {
+      // set auth headers to user auth token for credentials
+      Authorization: `Bearer ${authToken}`
+    }
+  };
+  // return data from axios POST request to the DB
+  //
+  return axios
+    .post(`${API_BASE_URL}/participants`, config, {
+      timeout: 3000
+    })
+    .then(res => {
+      // set response to a data variable that we destructure to access properties
+      // let { data } = res;
+      // successFn(data);
+      console.log(res);
+      return res;
+    })
+    .catch(err => {
+      console.log(err);
+      let { message } = err;
+      errorFn(message);
+      return err;
+    });
+};
+
+export default postParticipant;
--- main-app/client/src/components/IntakeForm/IntakeForm.jsx
+++ main-app/client/src/components/IntakeForm/IntakeForm.jsx
@@ -2,16 +2,35 @@ import * as React from "react";
 import IntakeFormQA from "./IntakeForm.data";
 import { DynamicFormContainer } from "components/DynamicForm";
 import "./IntakeForm.scss";
+import postParticipant from "../../api/postParticipant.api";
 
 class IntakeForm extends React.Component {
+  // submit handler for form
+  handleFormSubmit(formData) {
+    console.log("Form Submitted");
+    console.log(formData);
+    return postParticipant(formData, this.onSuccess, this.onError);
+  }
+  // on success of posting a new participant
+  // redirect user to new participant profile page?
+  // function onSuccess = (res) => {
+  //   console.log(res);
+  // }
+  // if an error occurs, show error at top of the form
+  onError = errorMessage => {
+    this.setState({ error: errorMessage, loading: false });
+  };
+  // form button component creates form data object
+  // form data is passed to container and handled in the handleSubmitForm
   render() {
     return (
       <div className="intake-form-container">
         <section className="intake-form">
           <h1 className="intake-form-title">Intake Form</h1>
+
           <DynamicFormContainer
             questions={IntakeFormQA}
-            // onSubmit={}
+            onSubmit={formData => this.handleFormSubmit(formData)}
             editable={true}
           />
         </section>
